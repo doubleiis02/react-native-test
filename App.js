8342f5e0f9de71627650bd22bb65f0e817777c57
Jiin <jiinkim02@gmail.com>
2021-03-18 12:16:49 -0700

    testing qr code generator

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <QRCode 
+        value="https://lahacks.com"
+      />
       <StatusBar style="auto" />
     </View>
   );
